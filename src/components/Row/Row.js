6fba35b5f4d7419b76f8e3c916611906f039e5be
Omar_Perez <omar1987perez2@gmail.com>
2022-12-29 18:32:09 +0100

    estilos en rows

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,3 +1,4 @@
+import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./Row.css";
@@ -17,17 +18,19 @@ const Row = ({ title, fetchUrl }) => {
   }, [fetchUrl]);
 
   return (
-    <div>
+    <div className="div-general">
       <h6 className="h4-row">{title}</h6>
       <div className="div-poster">
+      {/* <button className="botonDerecha" onClick={desplazarDerecha}></button> */}
         {movies.map((movie) => (
           <img
-            className="poster-row"
-            key={movie.id}
-            src={`${base_url}${movie.poster_path}`}
-            alt={movie.name}
+          className="poster-row"
+          key={movie.id}
+          src={`${base_url}${movie.poster_path}`}
+          alt={movie.name}
           ></img>
-        ))}
+          ))}
+          {/* <button className="botonIzquierda" onClick={desplazarIzquierda}></button> */}
       </div>
     </div>
   );
